897281f8cb3715dc187be423f36957cce648db4d
OnlyWooo <dngurwn@gmail.com>
2024-03-19 20:37:21 +0900

    메인에 토스트추가

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,14 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainScreen from './screens/MainScreen';
 import {RecoilRoot} from 'recoil';
+import ToastMessage from './components/toastMessage/ToastMessage';
 
 function App(): React.JSX.Element {
   return (
     <RecoilRoot>
       <NavigationContainer>
         <MainScreen />
+        <ToastMessage />
       </NavigationContainer>
     </RecoilRoot>
   );
